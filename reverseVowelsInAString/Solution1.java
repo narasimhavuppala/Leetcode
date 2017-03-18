1a54a2211c027093a4b69ab396614bc1d21f249f
Rakshith Kunchum <settyblue@gmail.com>
2017-03-17 19:04:00 -0700

    Solution to reverse vowels in a string.

---
+++ reverseVowelsInAString/Solution1.java
@@ -0,0 +1,22 @@
+public class Solution1 {
+    public String reverseVowels(String s) {
+        if(s == null || s.length() == 0)return "";
+        int n = s.length();
+        char[] vowels = new char[] {'a','e','i','o','u','A','E','I','O','U'};
+        HashSet<Character> set = new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+        int start = 0, end = n-1;
+        char[] sar = s.toCharArray();
+        while(start < end){
+            while(start<end && !set.contains(sar[start]))start++;
+            while(start<end && !set.contains(sar[end]))end--;
+            if(start<end){
+                char temp = sar[start];
+                sar[start] = sar[end];
+                sar[end] = temp;
+                start++;end--;
+            }
+        }
+        
+        return new String(sar);
+    }
+}
